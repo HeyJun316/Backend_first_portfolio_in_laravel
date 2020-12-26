d16a94fc5fcfad37dd80fa738c6c7ff8ba787005
HeyJun316 <55keke55@gmail.com>
2020-12-26 17:27:30 +0900

    ok

--- src/config/app.php
+++ src/config/app.php
@@ -138,9 +138,6 @@ return [
          * Laravel Framework Service Providers...
          */
 
-        //
-        Laravel\Socialite\SocialiteServiceProvider::class, // 'providers' 内に追記
-        //googleAPI ↑ariasaにも追記あり
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
@@ -190,8 +187,6 @@ return [
     */
 
     'aliases' => [
-        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
-        // 'aliases' 内に追記 googleapi↑providersにも追記あり
         'App' => Illuminate\Support\Facades\App::class,
         'Arr' => Illuminate\Support\Arr::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
