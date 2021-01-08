13e6e2b666886d33b5d2460d2be29e0583383947
HeyJun316 <55keke55@gmail.com>
2021-01-08 15:17:35 +0900

    あ

--- src/app/Http/Controllers/Auth/LoginController.php
+++ src/app/Http/Controllers/Auth/LoginController.php
@@ -38,7 +38,7 @@ class LoginController extends Controller
      */
     public function showLoginForm()
     {
-        return view('member.login');
+        return view('home.member.login');
     }
 
     public function logout()
